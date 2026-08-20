4e43cb05e85e9f02a95d4502a811876e201eef33
Muath Aloneq <moath.alonayq@gmail.com>
2026-08-20 14:46:32 +0300

    تبسيط الصفحة الرئيسية لتعرض فقط نظرة عامة على النادي والمتصدرين

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,29 +1,21 @@
 /* =========================================================
    controllers/homeController.js
-   منطق الصفحة الرئيسية: التعريف بالنادي + الإحصائيات + المتصدرون
+   منطق الصفحة الرئيسية: نظرة عامة على النادي + المتصدرون
    ========================================================= */
 
 const statsModel = require("../models/statsModel");
 const studentModel = require("../models/studentModel");
-const { getTodayQuestion } = require("../config/dailyQuestions");
 
 async function showHome(req, res, next) {
   try {
     const stats = await statsModel.getHomeStats();
-    const settings = await statsModel.getSettings();
     const topStudents = await studentModel.getTopStudents(10);
-    const clubDays = await statsModel.getClubDayNames();
-
-    const dailyQuestion = getTodayQuestion();
 
     res.render("home", {
       pageTitle: "الرئيسية",
       activeNav: "home",
       stats,
-      settings,
       topStudents,
-      clubDays,
-      dailyQuestion,
     });
   } catch (err) {
     next(err);
