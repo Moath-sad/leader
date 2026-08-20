b681c7b9557b7704686f3770f58901e1c6178d2e
Muath Aloneq <moath.alonayq@gmail.com>
2026-08-20 13:49:03 +0300

    إزالة ملف الفحص المؤقت

--- scripts_tmp_check.js
+++
@@ -1,7 +0,0 @@
-const mysql = require('mysql2/promise');
-(async () => {
-  const c = await mysql.createConnection(process.env.DATABASE_URL);
-  const [rows] = await c.query('SELECT name, category FROM `groups`');
-  console.log(JSON.stringify(rows));
-  await c.end();
-})();
